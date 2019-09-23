c6669acfc47d18ef548666d1a56692db5b008e5c
samah-gaber <samah.gaber.94@gmail.com>
2019-09-23 10:50:59 +0200

    Updates

---
+++ precache-manifest.f60bf215caf367fe4960b7d16e918d3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6816c85e85a7f70799490631c437ea8b",
+    "url": "/samah-gaber/Book-search-app.git/index.html"
+  },
+  {
+    "revision": "7db846a060b6b7b3dd7d",
+    "url": "/samah-gaber/Book-search-app.git/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7db846a060b6b7b3dd7d",
+    "url": "/samah-gaber/Book-search-app.git/static/js/2.f7795c49.chunk.js"
+  },
+  {
+    "revision": "babe5f367d11dda20083",
+    "url": "/samah-gaber/Book-search-app.git/static/js/main.a24d9614.chunk.js"
+  },
+  {
+    "revision": "dd59e6499fa075879906",
+    "url": "/samah-gaber/Book-search-app.git/static/js/runtime-main.dc37d348.js"
+  }
+]);
